1909b988802cfaca533e6d996295721786a9a7b8
siasungsieng <o1083027@gm.pu.edu.tw>
2021-04-19 11:54:04 +0800

    CustomView(自定義視圖) 、畫布、顏色及多點觸控

---
+++ app/src/main/java/tw/pu/edu/gm/o1083027/a20210419/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.pu.edu.gm.o1083027.a20210419
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(Fingers (this))
+    }
+}
---
+++ app/src/test/java/tw/pu/edu/gm/o1083027/a20210419/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package tw.pu.edu.gm.o1083027.a20210419
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
